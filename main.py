fc5453446d6776587b60c7d67ef07d2036eec832
iamashwin99 <f20171034@goa.bits-pilani.ac.in>
2022-02-18 12:51:21 +0530

    added helper file

--- main.py
+++ main.py
@@ -4,4 +4,5 @@ rm = pyvisa.ResourceManager()
 k6221=rm.open_resource("TCPIP::10.0.4.138::1394::SOCKET")
 k6221.read_termination = '\n'
 k6221.write_termination = '\n'
-k6221.query('*IDN?')
+print(k6221.query('*IDN?'))
+k6221.close()
