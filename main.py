80ad5ed5d6466d60492e3f679f4ecd2c4bae013f
iamashwin99 <f20171034@goa.bits-pilani.ac.in>
2022-02-21 15:34:42 +0530

    addedstreamlit boilerpalete code

--- main.py
+++ main.py
@@ -1,8 +1,39 @@
-import pyvisa
-from functions import *
+import os
+import streamlit as st
+import numpy as np
+from PIL import  Image
+from k6221 import *
+import plotly.express as px
+from measurements import dcon,delta,sinewave,lsweep,lsweep_delta
+
+# Custom imports 
+from multipage import MultiPage
+
+#Iport visa resource
 rm = pyvisa.ResourceManager()
-k6221=rm.open_resource("TCPIP::10.0.4.138::1394::SOCKET")
-k6221.read_termination = '\n'
-k6221.write_termination = '\n'
-print(k6221.query('*IDN?'))
-k6221.close()
+x = None
+
+address = "TCPIP::10.0.4.138::1394::SOCKET"
+dev = Keithley6221(address=address, rm=rm)
+
+
+
+# Create an instance of the app 
+app = MultiPage()
+
+# Title of the main page
+#display = Image.open('Logo.png')
+#display = np.array(display)
+col1, col2 = st.columns(2)
+col1.image('Logo.png', width = 300)
+col2.title("Differential Conductance Measurement Application")
+
+# Add all your application here
+app.add_page("Differential Conductance", dcon.app)
+app.add_page("Delta measurement", delta.app)
+app.add_page("Linear Sweep", lsweep.app)
+app.add_page("Sine Output", sinewave.app)
+
+
+# The main app
+app.run()
---
+++ measurements/lsweep_delta.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+
+# @st.cache
+def app():
+    st.markdown("## Linear sweep with delta measurement")
